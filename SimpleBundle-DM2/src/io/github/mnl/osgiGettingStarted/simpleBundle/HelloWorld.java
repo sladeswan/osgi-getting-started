69d4ab888bc81ef0205805cc29e64745b7c23c63
Michael N. Lipp <mnl@mnl.de>
2016-05-06 19:59:21 +0200

    Added dependency manager annotation example.

---
+++ SimpleBundle-DM2/src/io/github/mnl/osgiGettingStarted/simpleBundle/HelloWorld.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package io.github.mnl.osgiGettingStarted.simpleBundle;
+
+import org.apache.felix.dm.annotation.api.Component;
+import org.apache.felix.dm.annotation.api.ServiceDependency;
+import org.apache.felix.dm.annotation.api.Start;
+import org.apache.felix.dm.annotation.api.Stop;
+import org.osgi.service.log.LogService;
+
+/**
+ * @author mnl
+ *
+ */
+@Component
+public class HelloWorld implements Runnable {
+
+	@ServiceDependency(required=true)
+	private volatile LogService logService;
+
+	private Thread runner;
+	
+	@Start
+	public void start() {
+		runner = new Thread(this);
+		runner.start();
+	}
+	
+	@Stop
+	public void stop() {
+		runner.interrupt();
+		try {
+			runner.join();
+		} catch (InterruptedException e) {
+			logService.log(LogService.LOG_WARNING, 
+					"Could not terminate thread properly", e);
+		}
+	}
+	
+	@Override
+    public void run() {
+        System.out.println("Hello World!");
+        while (!runner.isInterrupted()) {
+            try {
+                logService.log(LogService.LOG_INFO, "Hello Word sleeping");
+                Thread.sleep (5000);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+
+}
