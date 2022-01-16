ab1cb0b7e1b8771f7e54101ae5bf738191f315d4
Michael N. Lipp <mnl@mnl.de>
2022-01-16 13:59:52 +0100

    Add sample property.

--- io.github.mnl.osgiGettingStarted.calculator.provider/src/io/github/mnl/osgiGettingStarted/calculator/provider/Activator.java
+++ io.github.mnl.osgiGettingStarted.calculator.provider/src/io/github/mnl/osgiGettingStarted/calculator/provider/Activator.java
@@ -1,7 +1,11 @@
 package io.github.mnl.osgiGettingStarted.calculator.provider;
 
+import java.util.Hashtable;
+import java.util.Map;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceRegistration;
 
 import io.github.mnl.osgiGettingStarted.calculator.Calculator;
@@ -13,7 +17,9 @@ public class Activator implements BundleActivator {
 	@Override
 	public void start(BundleContext context) throws Exception {
 		publishedService = context.registerService(
-				Calculator.class, new CalculatorImpl(), null);
+			Calculator.class, new CalculatorImpl(),
+			new Hashtable<>(
+					Map.of(Constants.SERVICE_VENDOR, "Michael N. Lipp")));
 	}
 
 	@Override
